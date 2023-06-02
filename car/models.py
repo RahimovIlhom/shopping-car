822d17976ffb12e1673e7392afc9e5e20c7ca4c9
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2023-06-02 11:44:51 +0500

    update model

--- car/models.py
+++ car/models.py
@@ -6,7 +6,7 @@ class Car(models.Model):
     brand = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.IntegerField(max_length=7)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     image = models.ImageField(null=True, blank=True, upload_to='car_images/')
